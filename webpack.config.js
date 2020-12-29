8745f22c0de3868db39a00d899b26b1783a89106
EvgenZagg <65083598+EvgenZagg@users.noreply.github.com>
2020-12-29 19:05:42 +0300

    Lesson_2

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,42 @@
-const path = require ('path');
-const webpack = require('webpack');
-
-
-module.exports = {
-    entry: {
-        app: './index.jsx'
+ const path = require('path');
+ const HtmlWebpackPlugin = require('html-webpack-plugin');
+ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+ 
+ module.exports = {
+   mode: 'development',
+   entry: {
+     index: './index.jsx'
+   },
+   context: path.resolve(__dirname, 'src'),
+   devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
     },
-    context: path.resolve (__dirname, 'src'),
-    output: {
-        path: path.resolve (__dirname, 'static', 'build'),
-        filename: 'app.js',
-        publicPath: path.resolve (__dirname, 'static', 'build')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                include: path.resolve (__dirname, 'src'),
-                exclude: path.resolve (__dirname, 'node_modules'),
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/env', '@babel/react']
-                }
-            }
-        ]
-    }
-}
+      module: {
+          rules: [
+              {
+                  test: /\.jsx?$/,
+                  include: path.resolve(__dirname, 'src'),
+                  exclude: path.resolve(__dirname, 'node_modules'),
+                  loader: 'babel-loader',
+                  options: {
+                      presets: ['@babel/env', '@babel/react'],
+                      plugins: ['@babel/plugin-proposal-class-properties']
+                  }
+              }
+          ]
+      },
+   plugins: [
+     new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+     new HtmlWebpackPlugin({
+       template: 'index.html'
+     }),
+   ],
+   output: {
+     filename: '[name].bundle.js',
+     path: path.resolve(__dirname, 'dist'),
+   },
+   resolve: {
+       extensions: ['.js', '.jsx']
+   }
+ };
