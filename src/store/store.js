c34d7a0651d819d164cd27a34c074d0dc4918edf
EvgenZagg <65083598+EvgenZagg@users.noreply.github.com>
2021-01-19 19:59:19 +0300

    Add files via upload

---
+++ src/store/store.js
@@ -0,0 +1,12 @@
+import { createStore } from 'redux';
+import initReducers from './reducers';
+
+
+export default function initStore() {
+   const innitialStore = {};
+
+   return createStore(
+       initReducers,
+       innitialStore,
+   );
+}
